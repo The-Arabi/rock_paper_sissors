82e159e26f33303f3f678d6f013c00279286f3e8
The-Arabi <100914809+The-Arabi@users.noreply.github.com>
2023-06-22 17:44:29 +0300

    Finish console based version of the game

--- javascirpt.js
+++ javascirpt.js
@@ -1,22 +1,22 @@
+//Define a statemenets matrix that tells the result
 let winningStatements = [
     ["It's a tie! Both chose rock","You Lose! Paper beats Rock","You win! rock beats sissors"],
     ["You win! Paper beats Rock","It's a tie! Both chose paper","You Lose! sissors beats paper"],
     ["You Lose! rock beats sissors","You win! sissors beats paper","It's a tie! Both chose sissors"]
 ]
-let playerChoice;
-
+//Aquire the choice of the computer as 0, 1, or 2
 function getComputerChoice() {
     return (Math.round(Math.random() * 10) % 3);
 }
-
+//log the result to the console via the matrix
 function play(computerChoice, playerChoice) {
     console.log(winningStatements[playerChoice][computerChoice])
 }
-
+//play five rounds of RPS
 function game() {
+    let playerChoice;
     for (i = 0; i < 5; i++) {
-    playerChoice = prompt('rock, paper, or sissors?')
-    playerChoice = playerChoice.toLowerCase()
+    playerChoice = prompt('rock, paper, or sissors?').toLowerCase()
     playerChoice == 'rock'? playerChoice = 0: playerChoice == 'paper'? playerChoice = 1: playerChoice = 2;
     play(getComputerChoice(), playerChoice)
     }
