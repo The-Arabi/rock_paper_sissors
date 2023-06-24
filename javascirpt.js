ac5d1a0bb9acad2d44b8ac591720a4a3844a5800
The-Arabi <100914809+The-Arabi@users.noreply.github.com>
2023-06-24 18:14:01 +0300

    Finish scoreboard and winningstatement display

--- javascirpt.js
+++ javascirpt.js
@@ -1,16 +1,23 @@
-//Define a statemenets matrix that tells the result
+//Define needed variabels
 let winningStatements = [
-    ["It's a tie! Both chose rock","You Lose! Paper beats Rock","You win! rock beats sissors"],
-    ["You win! Paper beats Rock","It's a tie! Both chose paper","You Lose! sissors beats paper"],
-    ["You Lose! rock beats sissors","You win! sissors beats paper","It's a tie! Both chose sissors"]
+    ["It's a tie! Both chose rock","You lose! Paper beats Rock","You win! rock beats sissors"],
+    ["You win! Paper beats Rock","It's a tie! Both chose paper","You lose! sissors beats paper"],
+    ["You lose! rock beats sissors","You win! sissors beats paper","It's a tie! Both chose sissors"]
 ]
+let buttons = document.querySelectorAll('button')
+let div = document.querySelector('div')
+let pscore = 0;
+let cscore = 0;
+let statement;
 //Aquire the choice of the computer as 0, 1, or 2
 function getComputerChoice() {
     return (Math.round(Math.random() * 10) % 3);
 }
 //play a round of rps
 function play(computerChoice, playerChoice) {
-    console.log(winningStatements[playerChoice][computerChoice])
+    statement = winningStatements[playerChoice][computerChoice];
+    statement.substring(4,7) == "win"? pscore++: statement.substring(4,8) == "lose"? cscore++: null;
+    div.innerHTML = `${statement} <br> player score: ${pscore} <br> computer score: ${cscore}`
 }
 //initiate a round of rps upon selecting a choice
 function game() {
@@ -18,5 +25,4 @@ function game() {
     play(getComputerChoice(), playerChoice)
 }
 //detect player selecting a choice
-let buttons = document.querySelectorAll('button')
 buttons.forEach(button => {button.addEventListener("click", game)});
